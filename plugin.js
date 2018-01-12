0d9030b7a4f61049dac924edc33fa096143aaf45
triptolemusew <deneuve.crepes@gmail.com>
2018-01-13 05:04:30 +0800

    Added "Download All" function.

--- plugin.js
+++ plugin.js
@@ -3,14 +3,34 @@ var count = 0;
 var content = {};
 var tab_title = '';
 
+function download_All(list){
+	var link;
+	for (var i = 0; i < list.length; i++)
+	{
+		link = document.createElement("a");
+		link.download = "one";
+		link.href = list[i];
+		link.click();
+	}
+}
+
 function display_DOM(results){
 	h1 = results;
-	console.log(h1[0][1]);
-	var link = document.createElement("a");
-	link.download = "one";
-	link.href = h1[0][1];
-	link.click();
-  	document.querySelector("#ids").innerHTML = "<p>tab title: " + tab_title + "</p><p>dom h1: " + h1 + " </p>";
+  	document.querySelector("#ids").innerHTML = "<p>Thread: " + tab_title + "<br>";
+  	document.querySelector("#numberImages").innerHTML = "No. of Images: " + h1[0].length;
+  	var node;
+  	var textnode;
+  	console.log(h1[0].length);
+  	for (var i = 0; i < h1[0].length; i++)
+  	{
+  		node = document.createElement("li");
+  		linknode = document.createElement("a");
+  		linknode.href = h1[0][i];
+  		linknode.textContent = h1[0][i];
+  		node.appendChild(linknode);
+  		document.getElementById("List").appendChild(node);
+  	}
+  	document.getElementById("DownloadAll").onclick = function() {download_All(h1[0])};
 }
 
 chrome.runtime.onMessage.addListener(function(request, sender){
@@ -20,18 +40,15 @@ chrome.runtime.onMessage.addListener(function(request, sender){
 });
 
 function checkPage(){
-	//var popupDiv = document.getElementById(divName);
 	var message = document.querySelector('#message');
-
 	var button = document.getElementById('checkPage');
 	var text = document.getElementById('numberImages');
   	var elems = document.body.getElementsByTagName("body");
   	var counts = document.getElementsByClassName("dialog");
   	var body = document.body.children;
   	var bun = document.querySelectorAll('button');
+  	var a = document.getElementById("DownloadAll");
 	button.addEventListener('click', function(){
-		//text.textContent = "das";
-
 		// chrome.tabs.getSelected(null, function(tab){
 		// });
 		//Getting the media files count
@@ -39,7 +56,6 @@ function checkPage(){
 			count++;	
 		}
 
-		text.textContent = "asadsd";
 		//text.appendChild(document.createTextNode(count));
 		// text.textContent = bun[0].innerHTML;
 
